dae352001cbcebcb58d722786a9bc97165ca18c4
msarti <simone.sarti@leonardo.com>
2024-10-15 17:59:54 +0200

    imporved test and inference

--- inference.py
+++ inference.py
@@ -1,195 +1,89 @@
-import os
-import re
 from argparse import ArgumentParser
 from pathlib import Path
-from typing import Union
 
-from dotenv import load_dotenv
 from ultralytics import YOLO
 
-import wandb
-
-ALLOWED_IMAGE_FORMATS = [".bmp", ".dng", ".jpeg", ".jpg", ".mpo", ".png", ".tif", ".tiff", ".webp", ".pfm"]
-ALLOWED_VIDEO_FORMATS = [".asf", ".avi", ".gif", ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".mpg", ".ts", ".wmv",
-                         ".webm"]
+from src.configs.cli import check_inference_arguments
 
 
 def parse_cli_arguments():
     parser = ArgumentParser()
 
-    parser.add_argument("--data_path", required=True, type=str)
-    parser.add_argument("--checkpoint", required=True, type=str)
-    parser.add_argument("--output_dir", required=True, type=str)
+    parser.add_argument("--data_path", type=str, required=True,
+                        help="Path to an image, a video, a folder containing images or videos, or a youtube link")
+    parser.add_argument("--checkpoint", type=str, required=True,
+                        help="Path to a pretrained YOLO model checkpoint (.pt)")
+    parser.add_argument("--output_dir", type=str, required=True,
+                        help="Path where to save the inference results within ./experiments")
 
-    parser.add_argument('--height', type=int, default=640, help="Image/video height for training (default: 640)")
-    parser.add_argument('--width', type=int, default=640, help="Image/video width for training (default: 640)")
-    parser.add_argument('--batch_size', type=int, default=16, help="Batch size for testing (default: 16)")
-    parser.add_argument('--conf_thresh', type=float, default=0.25,
+    parser.add_argument('--height', type=int, default=640,
+                        help="Image/video height for training (default: 640)")
+    parser.add_argument('--width', type=int, default=640,
+                        help="Image/video width for training (default: 640)")
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help="Batch size for testing (default: 16)")
+
+    parser.add_argument('--conf', type=float, default=0.25,
                         help="Confidence threshold for detecting objects (default: 0.25)")
-    parser.add_argument('--iou_thresh', type=float, default=0.70,
+    parser.add_argument('--iou', type=float, default=0.70,
                         help="IoU threshold for NMS (Non-Max Suppression) (default: 0.70)")
 
-    parser.add_argument("--run_name", type=str, required=True)
+    # TODO FIX DEVICE can be string, list, int
+    parser.add_argument("--device", type=int, default=3,
+                        help="device where the model is run, can be 'cpu' a number for the corresponding gpu, "
+                             "for a list of numbers for distributed inference")
 
     args = parser.parse_args()
 
     return args
 
 
-def is_in_01(metric: float):
-    return 0 <= metric <= 1
+def main():
+    args = parse_cli_arguments()
 
+    media_type = check_inference_arguments(args)
 
-def is_valid_checkpoint(checkpoint: Path):
-    return checkpoint.exists() and checkpoint.suffix == ".pt"
+    data_path = Path(args.data_path) if media_type != "youtube" else args.data_path
 
-
-def is_youtube_link(url: str):
-    # Regular expression pattern for YouTube URLs
-    youtube_regex = re.compile(
-        r'^(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/'
-        r'(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
-
-    # Match the URL against the pattern
-    match = youtube_regex.match(url)
-
-    return bool(match)
-
-
-def is_valid_image(file: Path):
-    return file.suffix.lower() in ALLOWED_VIDEO_FORMATS
-
-
-def is_valid_video(file: Path):
-    return file.suffix.lower() in ALLOWED_VIDEO_FORMATS
-
-
-def is_valid_images_dir(files: list[Path]):
-    return len(files) > 0 and all(file.suffix.lower() in ALLOWED_IMAGE_FORMATS for file in files)
-
-
-def is_valid_videos_dir(files: list[Path]):
-    return len(files) > 0 and all(file.suffix.lower() in ALLOWED_VIDEO_FORMATS for file in files)
-
-
-def check_cli_arguments(args):
-    if not is_in_01(args.conf):
-        print(f"ERROR: confidence must be in [0,1]. Got {args.conf}")
-        exit()
-
-    if not is_in_01(args.iou):
-        print(f"ERROR: iou must be in [0,1]. Got {args.iou}")
-        exit()
-
-    model_checkpoint = Path(args.checkpoint)
-    if not is_valid_checkpoint(model_checkpoint):
-        print(f"ERROR: model checkpoint {args.checkpoint} was not found.")
-        exit()
-
-    data_path = args.data_path
-
-    if is_youtube_link(data_path):
-        return "youtube"
-
-    else:
-        data_path = Path(data_path)
-
-        if args.data_path.isdir():
-
-            files = [file for file in data_path.iterdir() if file.is_file()]
-
-            if is_valid_images_dir(files):
-                return "images"
-            if is_valid_videos_dir(files):
-                return "videos"
-
-        elif args.data_path.isfile():
-            if is_valid_image(data_path):
-                return "image"
-            if is_valid_video(data_path):
-                return "video"
-
-    print(f"ERROR: ({data_path}) is not an image, video, folder containing them, nor a youtube link")
-    exit()
-
-
-def run_inference(
-        model,
-        media_type: str,
-        data_path: Union[Path, str],
-        output_dir: Path,
-        imgsz: tuple[float, float],
-        conf: float,
-        iou: float,
-        device: str,
-):
-    """
-    Run YOLO11 inference on the given input source (image, video, or YouTube URL).
-
-    :param imgsz:
-    :param model:
-    :param media_type:
-    :param data_path:
-    :param output_dir:
-    :param conf:
-    :param iou:
-    :param device:
-    """
-
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-    if media_type in ["images", "videos", "youtube"]:
-        source = [file for file in data_path.iterdir() if file.is_file()]
-        stream = True
-    else:
-        source = data_path
+    if media_type in ["image"]:
         stream = False
+    else:  # media_type in ["images", "video", "videos", "youtube"]
+        stream = True
 
-    model.predict(
-        source=source,
+    model = YOLO(args.checkpoint)
+
+    results = model.predict(
+        source=data_path,
         classes=[0],
-        imgsz=imgsz,
-        conf=conf,
-        iou=iou,
-        device=device,
+        imgsz=(args.height, args.width),
+        conf=args.conf,
+        iou=args.iou,
         stream=stream,
         save=True,
-        save_dir=output_dir,
+        project="experiments",
+        name=args.output_dir,
+        device=args.device,
     )
 
+    if stream:
+        print("saving with stream")
+        # iterate through generator to trigger saving of results
+        for r in results:
+            pass
 
-def main():
-    # Load environment variables from .env file
-    load_dotenv()
-    wandb_api_key = os.getenv("WANDB_API_KEY")
-
-    # Ensure W&B API key is loaded
-    if wandb_api_key is None:
-        raise ValueError("W&B API key not found. Please check your .env file.")
-
-    args = parse_cli_arguments()
+    # TODO log results
+    """
+    wandb_api_key = get_wandb_api_key()
 
     wandb.login(key=wandb_api_key)
     wandb.init(
-        project="experiments",
-        name=args.run_name,
+        project="agrarian",
+        name="test_inference",
     )
-
-    media_type = check_cli_arguments(args)
-    model_checkpoint = args.checkpoint
-    data_path = Path(args.data_path) if media_type != "youtube" else args.data_path
-    output_dir = Path(args.output_dir)
-    imgsz = (args.height, args.width)
-    conf = args.conf
-    iou = args.iou
-    device = args.device
-
-    model = YOLO(model_checkpoint)
-
-    run_inference(model, media_type, data_path, output_dir, imgsz, conf, iou, device)
 
     # Finish wandb logging
     wandb.finish()
+    """
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,56 +1,71 @@
 import argparse
-import os
-
-from dotenv import load_dotenv
+from src.logging.utils import get_wandb_api_key
 from ultralytics import YOLO
+from pathlib import Path
 
 import wandb
 
 
 # Parse command-line arguments using argparse
 def parse_args():
-    parser = argparse.ArgumentParser(description="YOLOv8 Testing and Evaluation with W&B")
+    parser = argparse.ArgumentParser()
 
-    # Model and dataset paths
-    parser.add_argument('--model', type=str, required=True,
-                        help="Path to the trained YOLO11 model or checkpoint (e.g., best.pt)")
-    parser.add_argument('--data', type=str, required=True, help="Path to the dataset YAML file (for testing dataset)")
+    parser.add_argument('--data', type=str, required=True,
+                        help="Path to the dataset YAML configuration file")
     parser.add_argument('--split', type=str, default="test", choices=["train", "val", "test"],
-                        help="Path to the dataset YAML file (for testing dataset)")
-    parser.add_argument('--run_name', type=str, default="yolo11_test",
-                        help="Name of the test run in W&B (default: yolo11_test)")
+                        help="dataset split on which to validate the model (default: test)")
 
-    # Test-specific parameters
-    parser.add_argument('--imgsz', type=int, default=640, help="Image/video size for training (default: 640)")
-    parser.add_argument('--batch_size', type=int, default=16, help="Batch size for testing (default: 16)")
-    parser.add_argument('--conf_thresh', type=float, default=0.25,
+    parser.add_argument("--checkpoint", type=str, required=True,
+                        help="Path to a pretrained YOLO model checkpoint (.pt)")
+
+    parser.add_argument("--output_dir", type=str, required=True,
+                        help="Path where to save the inference results within ./experiments")
+
+    parser.add_argument('--imgsz', type=int, default=640,
+                        help="Image/video size for training (default: 640)")
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help="Batch size for testing (default: 16)")
+
+    parser.add_argument('--conf', type=float, default=0.25,
                         help="Confidence threshold for detecting objects (default: 0.25)")
-    parser.add_argument('--iou_thresh', type=float, default=0.70,
+    parser.add_argument('--iou', type=float, default=0.70,
                         help="IoU threshold for NMS (Non-Max Suppression) (default: 0.70)")
 
-    parser.add_argument('--device', type=str, default="3", help="device to use for training (default 3)")
+    # TODO FIX DEVICE can be string, list, int
+    parser.add_argument("--device", type=int, default=3,
+                        help="device where the model is run, can be 'cpu' a number for the corresponding gpu, "
+                             "for a list of numbers for distributed inference")
 
     return parser.parse_args()
 
 
 def main():
-
-    # Load environment variables from .env file
-    load_dotenv()
-    wandb_api_key = os.getenv("WANDB_API_KEY")
-
-    # Ensure W&B API key is loaded
-    if wandb_api_key is None:
-        raise ValueError("W&B API key not found. Please check your .env file.")
-
     # Parse the command-line arguments
     args = parse_args()
+
+    # Load the trained YOLOv11 model
+    model = YOLO(args.checkpoint)
+
+    # Perform validation (which includes evaluation on the test set)
+    results = model.val(
+        data=Path(args.data),
+        split=args.split,
+        imgsz=args.imgsz,
+        batch=args.batch_size,
+        conf=args.conf,
+        iou=args.iou,
+        device=args.device,
+    )
+
+    # TODO log results
+    """
+    wandb_api_key = get_wandb_api_key()
 
     # Initialize W&B logging
     wandb.login(key=wandb_api_key)
     wandb.init(
-        project="experiments",
-        name=args.run_name
+        project="agrarian",
+        name="test_test"
     )
 
     # Log test parameters to W&B
@@ -60,29 +75,14 @@ def main():
         "split": args.split,
         "imgsz": args.imgsz,
         "batch_size": args.batch_size,
-        "conf_thresh": args.conf_thresh,
-        "iou_thresh": args.iou_thresh,
+        "conf": args.conf,
+        "iou": args.iou,
     })
-
-    # Load the trained YOLOv8 model
-    model = YOLO(args.model)
-
-    # Perform validation (which includes evaluation on the test set)
-    results = model.val(
-        data=args.data,  # Path to the dataset YAML file
-        split=args.split,
-        imgsz=args.imgsz,  # Image size for evaluation
-        batch=args.batch_size,  # Batch size for evaluation
-        conf=args.conf_thresh,  # Confidence threshold for detection
-        iou=args.iou_thresh,  # IoU threshold for NMS
-        device=args.device,
-        verbose=False,  # Print detailed logs during evaluation
-    )
 
     # Log validation metrics to W&B
     wandb.log(dict(results.results_dict))
     wandb.log({
-        "box_mAP50-95": results.box.map, # mAP50-95
+        "box_mAP50-95": results.box.map,  # mAP50-95
         "box_mAP50": results.box.map50,  # mAP50
         "box_mAP75": results.box.map75,  # mAP75
         "box_cat_mAP50-95": results.box.maps,  # list of mAP50-95 for each category)
@@ -90,8 +90,7 @@ def main():
 
     # Finish the W&B run
     wandb.finish()
-
-    print(results)
+    """
 
 
 if __name__ == "__main__":
